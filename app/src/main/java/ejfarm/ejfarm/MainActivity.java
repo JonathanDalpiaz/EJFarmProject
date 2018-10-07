1357cbe669d562d875468c40fd6a8165a674f304
Jonathan Dalpiaz <dalpiazj@gmail.com>
2018-10-07 16:10:30 -0300

    visual

--- app/src/main/java/ejfarm/ejfarm/MainActivity.java
+++ app/src/main/java/ejfarm/ejfarm/MainActivity.java
@@ -2,6 +2,11 @@ package ejfarm.ejfarm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AndroidException;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listaOpcoes = (ListView) findViewById(R.id.ListaFuncoes);
+
+        ArrayList<String> opcoes = selecionarOpcao();
+
+        ArrayAdapter<String> arrayAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, opcoes);
+
     }
+
+    private ArrayList<String> selecionarOpcao(){
+        ArrayList<String> opcao = new ArrayList<>();
+        opcao.add("Plantio Em Tempo Real");
+        opcao.add("Outros Plantios");
+        opcao.add("Sobre o Aplicativo");
+        opcao.add("Sair");
+        return opcao;
+    }
+
 }
